1a5b94f8e8ef9ceacb74f41113fe3960f185d8b0
Jack Watters <jawatter@e1z2r9p25.42.us.org>
2019-05-08 18:05:02 -0700

    update

--- main.c
+++ main.c
@@ -1,15 +1,39 @@
 #include "ft_ls.h"
 #include <stdio.h>
 
-int     convert_flags(char **av, int *flags)
+int     count_flag(char **av)
+{
+    int x;
+    int y;
+    int ret;
+    
+    x = 1;
+    ret = 0;
+    while (av[x])
+    {
+        if (av[x][0] == '-')
+        {
+            y = 1;
+            while (av[x][y])
+            {
+                ret++;
+                y++;
+            } 
+        }
+        x++;
+    }
+    return (ret);
+}
+
+char     *convert_flags(char **av, int *flags)
 {
     int         x;
     int         y;
     int         z;
     char        *ret;
-    if (!(count_flag(av)))
+    if (!(*flags = count_flag(av)))
         return (NULL);
-    if (!(ret = (char *)malloc(sizeof(char) + 1)))
+    if (!(ret = (char *)malloc(sizeof(&flags) + 1)))
         exit(1);
     x = 1;
     y = 0;
@@ -19,11 +43,23 @@ int     convert_flags(char **av, int *flags)
         {
             z = 1;
             //start at 2nd char of 2d array and copy it into ret[y]
-
-
+            if (av[x][z])
+            {
+                ret[y] = av[x][z];
+                y++;
+                z++;
+            }
         }
+           x++;
     }
+    printf("%s\n", ret);
+    return (ret);
 }
+
+// char    **get_dir(char **av, int dir)
+// {
+
+// }
 
 int     main(int ac, char **av) 
 {
@@ -42,12 +78,13 @@ int     main(int ac, char **av)
             exit(1);
         sp->p_dir[0][0] = '.';
         sp->p_dir[1] = NULL;
+        printf("%s\n", sp->p_dir[0]);
     }
     else
     {
-        sp->p_flags = convert_flags(av, sp->ls_flags);
+        sp->p_flags = convert_flags(av, &sp->ls_flags);
+        // sp->p_dir = get_dir(av, &sp->ls_dir);
         printf("%s\n", sp->p_flags);
     }
-    printf("%s\n", sp->p_dir[0]);
     return (0);
 }
